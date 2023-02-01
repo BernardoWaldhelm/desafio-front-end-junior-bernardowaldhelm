e037927b4f44ddb474a5b7cb7e7227e0b6d70fb3
Bernardo Waldhelm <ernaniwaldhelm@gmail.com>
2023-02-01 00:32:34 -0300

    feat(home): adicionando modal de formulário enviado com sucesso

--- desafio-front/src/components/molecules/FormCadastro/FormCadastro.tsx
+++ desafio-front/src/components/molecules/FormCadastro/FormCadastro.tsx
@@ -1,6 +1,6 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import MaskedInput from "react-text-mask";
-import { useState } from "react";
+import React, { useState } from "react";
 
 import FormSchema from "../../../schema/FormSchema";
 import { ButtonForm } from "../../Atoms/ButtonForm/ButtonForm";
@@ -28,7 +28,30 @@ const initialValues = {
 };
 
 const FormCadastro = () => {
-  const [FormSuccess, setFormiSuccess] = useState(false);
+  const [FormSuccess, setFormSuccess] = useState(false);
+
+  const [screen, setScreen] = React.useState<"login" | "personal" | "address">(
+    "login"
+  );
+
+  const handleNavigateLogin = () => {
+    setScreen("login");
+  };
+  const handleNavigatePersonal = () => {
+    setScreen("personal");
+  };
+
+  const handleNavigateAddress = () => {
+    setScreen("address");
+  };
+
+  const handleSuccess = () => {
+    const modal: HTMLElement | undefined | null =
+      document.getElementById("modal");
+    if (modal !== undefined && modal !== null) {
+      modal!.style.display = "none";
+    }
+  };
 
   return (
     <div className={styles["form"]}>
@@ -37,154 +60,246 @@ const FormCadastro = () => {
           console.log(values);
           onSubmitProps.resetForm();
           onSubmitProps.setSubmitting(false);
-          setFormiSuccess(true);
+          setFormSuccess(true);
+          const modal: HTMLElement | undefined | null =
+            document.getElementById("modal");
+          if (modal !== undefined && modal !== null) {
+            modal!.style.display = "flex";
+          }
         }}
         initialValues={initialValues}
         validationSchema={FormSchema}
       >
         {() => (
-          <Form className={styles["form__wrapper"]}>
-            <h2 className={styles["form__title"]}>Preencha o formulário </h2>
-            <div className={styles["form__col"]}>
-              <LabelForm
-                className={styles["form__col__type"]}
-                htmlFor={"email"}
-                text={"E-mail"}
-              />
-              <Field id="email" name="email" placeholder="Seu email" />
-              <ErrorMessage
-                component="span"
-                name="email"
-                className={styles["form__col__error"]}
-              />
+          <>
+            <div className={styles["form__navigate"]}>
+              <button
+                type="button"
+                className={styles["form__navigate__button"]}
+                onClick={handleNavigateLogin}
+              >
+                Etapa 1
+              </button>
+              <button
+                type="button"
+                className={styles["form__navigate__button"]}
+                onClick={handleNavigatePersonal}
+              >
+                Etapa 2
+              </button>
+              <button
+                type="button"
+                className={styles["form__navigate__button"]}
+                onClick={handleNavigateAddress}
+              >
+                Etapa 3
+              </button>
             </div>
-            <div className={styles["form__col"]}>
-              <LabelForm
-                className={styles["form__col__type"]}
-                htmlFor={"password"}
-                text={"Senha"}
-              />
-              <Field id="password" name="password" placeholder="Sua senha" />
-              <ErrorMessage
-                component="span"
-                name="password"
-                className={styles["form__col__error"]}
-              />
-            </div>
-            <div className={styles["form__col"]}>
-              <LabelForm
-                className={styles["form__col__type"]}
-                htmlFor={"confirmationPassword"}
-                text={"Confirmação de Senha"}
-              />
-              <Field
-                id="confirmationPassword"
-                name="confirmationPassword"
-                placeholder="Confirme sua senha"
-              />
-              <ErrorMessage
-                component="span"
-                name="confirmationPassword"
-                className={styles["form__col__error"]}
-              />
-            </div>
-            <div className={styles["form__col"]}>
-              <LabelForm
-                className={styles["form__col__type"]}
-                htmlFor={"name"}
-                text={"Nome"}
-              />
-              <Field id="name" name="name" placeholder="Seu nome" />
-              <ErrorMessage
-                component="span"
-                name="name"
-                className={styles["form__col__error"]}
-              />
-            </div>
-            <div className={styles["form__col"]}>
-              <LabelForm
-                className={styles["form__col__type"]}
-                htmlFor={"lastName"}
-                text={"Sobrenome"}
-              />
-              <Field
-                id="lastName"
-                name="lastName"
-                placeholder="Seu sobrenome"
-              />
-              <ErrorMessage
-                component="span"
-                name="lastName"
-                className={styles["form__col__error"]}
-              />
-            </div>
-            <div className={styles["form__col"]}>
-              <LabelForm
-                className={styles["form__col__type"]}
-                htmlFor={"birthDate"}
-                text={"Data de Nascimento"}
-              />
+            <Form className={styles["form__wrapper"]}>
+              <h2 className={styles["form__title"]}>Preencha o formulário </h2>
+              {screen === "login" && (
+                <>
+                  <div className={styles["form__col"]}>
+                    <LabelForm
+                      className={styles["form__col__type"]}
+                      htmlFor={"email"}
+                      text={"E-mail"}
+                    />
+                    <Field id="email" name="email" placeholder="Seu email" />
+                    <ErrorMessage
+                      component="span"
+                      name="email"
+                      className={styles["form__col__error"]}
+                    />
+                  </div>
+                  <div className={styles["form__col"]}>
+                    <LabelForm
+                      className={styles["form__col__type"]}
+                      htmlFor={"password"}
+                      text={"Senha"}
+                    />
+                    <Field
+                      id="password"
+                      name="password"
+                      placeholder="Sua senha"
+                    />
+                    <ErrorMessage
+                      component="span"
+                      name="password"
+                      className={styles["form__col__error"]}
+                    />
+                  </div>
+                  <div className={styles["form__col"]}>
+                    <LabelForm
+                      className={styles["form__col__type"]}
+                      htmlFor={"confirmationPassword"}
+                      text={"Confirmação de Senha"}
+                    />
+                    <Field
+                      id="confirmationPassword"
+                      name="confirmationPassword"
+                      placeholder="Confirme sua senha"
+                    />
+                    <ErrorMessage
+                      component="span"
+                      name="confirmationPassword"
+                      className={styles["form__col__error"]}
+                    />
+                  </div>
+                  <div className={styles["form__button__back-go"]}>
+                    <button
+                      type="button"
+                      className={styles["form__button__back-go__button"]}
+                      onClick={handleNavigatePersonal}
+                    >
+                      Avançar
+                    </button>
+                  </div>
+                </>
+              )}
+              {screen === "personal" && (
+                <>
+                  <div className={styles["form__col"]}>
+                    <LabelForm
+                      className={styles["form__col__type"]}
+                      htmlFor={"name"}
+                      text={"Nome"}
+                    />
+                    <Field id="name" name="name" placeholder="Seu nome" />
+                    <ErrorMessage
+                      component="span"
+                      name="name"
+                      className={styles["form__col__error"]}
+                    />
+                  </div>
+                  <div className={styles["form__col"]}>
+                    <LabelForm
+                      className={styles["form__col__type"]}
+                      htmlFor={"lastName"}
+                      text={"Sobrenome"}
+                    />
+                    <Field
+                      id="lastName"
+                      name="lastName"
+                      placeholder="Seu sobrenome"
+                    />
+                    <ErrorMessage
+                      component="span"
+                      name="lastName"
+                      className={styles["form__col__error"]}
+                    />
+                  </div>
+                  <div className={styles["form__col"]}>
+                    <LabelForm
+                      className={styles["form__col__type"]}
+                      htmlFor={"birthDate"}
+                      text={"Data de Nascimento"}
+                    />
 
-              <Field
-                type="tel"
-                name="birthDate"
-                render={({ field }: any) => (
-                  <MaskedInput
-                    id="birthDate"
-                    type="tel"
-                    mask={[
-                      /[0-3]/,
-                      /[0-9]/,
-                      ".",
-                      /[0-1]/,
-                      /[0-9]/,
-                      ".",
-                      /[0-9]/,
-                      /[0-9]/,
-                      /[0-9]/,
-                      /[0-9]/,
-                    ]}
-                    {...field}
-                    placeholder="00.00.0000"
-                  />
-                )}
-              />
-              <ErrorMessage
-                component="span"
-                name="birthDate"
-                className={styles["form__col__error"]}
-              />
-            </div>
-            <div className={styles["form__col"]}>
-              <LabelForm
-                className={styles["form__col__type"]}
-                htmlFor={"address"}
-                text={"Endereço completo"}
-              />
-              <Field
-                id="address"
-                name="address"
-                placeholder="Endereço completo"
-              />
-              <ErrorMessage
-                component="span"
-                name="address"
-                className={styles["form__col__error"]}
-              />
-            </div>
-
-            <ButtonForm
-              className={styles["form__button"]}
-              type="submit"
-              text={"CADASTRE-SE"}
-            />
-
-            {FormSuccess && (
-              <span className={styles["form__wrapper__success"]}>
-                *Formulário enviado com sucesso!
-              </span>
-            )}
-          </Form>
+                    <Field
+                      type="tel"
+                      name="birthDate"
+                      render={({ field }: any) => (
+                        <MaskedInput
+                          id="birthDate"
+                          placeholder="00.00.0000"
+                          type="tel"
+                          mask={[
+                            /[0-3]/,
+                            /[0-9]/,
+                            ".",
+                            /[0-1]/,
+                            /[0-9]/,
+                            ".",
+                            /[0-9]/,
+                            /[0-9]/,
+                            /[0-9]/,
+                            /[0-9]/,
+                          ]}
+                          {...field}
+                        />
+                      )}
+                    ></Field>
+                    <ErrorMessage
+                      component="span"
+                      name="birthDate"
+                      className={styles["form__col__error"]}
+                    />
+                  </div>
+                  <div className={styles["form__button__back-go"]}>
+                    <button
+                      type="button"
+                      className={styles["form__button__back-go__button"]}
+                      onClick={handleNavigateLogin}
+                    >
+                      Voltar
+                    </button>
+                    <button
+                      type="button"
+                      className={styles["form__button__back-go__button"]}
+                      onClick={handleNavigateAddress}
+                    >
+                      Avançar
+                    </button>
+                  </div>
+                </>
+              )}
+              {screen === "address" && (
+                <>
+                  <div className={styles["form__col"]}>
+                    <LabelForm
+                      className={styles["form__col__type"]}
+                      htmlFor={"address"}
+                      text={"Endereço completo"}
+                    />
+                    <Field
+                      id="address"
+                      name="address"
+                      placeholder="Endereço completo"
+                    />
+                    <ErrorMessage
+                      component="span"
+                      name="address"
+                      className={styles["form__col__error"]}
+                    />
+                  </div>
+                  <div className={styles["form__button__back-go"]}>
+                    <button
+                      type="button"
+                      className={styles["form__button__back-go__button"]}
+                      onClick={handleNavigatePersonal}
+                    >
+                      Voltar
+                    </button>
+                  </div>
+                </>
+              )}
+              <div className={styles["form__button__back-go"]}>
+                <ButtonForm
+                  className={styles["form__button"]}
+                  type="submit"
+                  text={"Enviar"}
+                  disabled={false}
+                />
+              </div>
+              {FormSuccess && (
+                <div id="modal" className={styles["form__modal"]}>
+                  <div className={styles["form__modal__center"]}>
+                    <span className={styles["form__wrapper__success"]}>
+                      *Formulário enviado com sucesso!
+                    </span>
+                    <button
+                      className={styles["form__modal__center__button"]}
+                      type="button"
+                      onClick={handleSuccess}
+                    >
+                      x
+                    </button>
+                  </div>
+                </div>
+              )}
+            </Form>
+          </>
         )}
       </Formik>
     </div>
--- desafio-front/src/schema/FormSchema.ts
+++ desafio-front/src/schema/FormSchema.ts
@@ -7,6 +7,6 @@ export default Yup.object().shape({
   confirmationPassword: Yup.string().required("*Campo Obrigatório").min(6, "*No mínimo 3 digitos").oneOf([Yup.ref('password'), null], 'Confirmação incorreta'),
   name: Yup.string().required("*Campo Obrigatório").min(3, "*No mínimo 3 digitos"),
   lastName: Yup.string().required("*Campo Obrigatório").min(3, "*No mínimo 3 digitos"),
-  birthDate: Yup.string().matches(birthDay, "Data Inválida").required("*Campo Obrigatório"),
-  adrress: Yup.string().required('*Campo Obrigatório'),
+  birthDate: Yup.string().matches(birthDay, "Data Inválida"),
+  address: Yup.string().required('*Campo Obrigatório'),
 })
